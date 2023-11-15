b3695c7f95c261790191d952945a4dc4817e8ea9
Mikhail Ulanov <mixaulanoff@yandex.ru>
2023-11-15 23:55:53 +0300

    Swap on postgreesql

--- WebHomework/WebHomework/settings.py
+++ WebHomework/WebHomework/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'WebHomework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mikeulanov',
+        'USER': 'mikeulanov',
+        'PASSWORD': 'mikeulanov',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
